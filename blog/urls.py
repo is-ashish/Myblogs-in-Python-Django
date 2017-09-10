cac0155a8db1921658ddf6368cf5ac9e9bad54e2
Ashish Jain <ashishjainjnp18@gmail.com>
2017-09-10 21:53:55 +0530

    change

--- blog/urls.py
+++ blog/urls.py
@@ -8,11 +8,11 @@ urlpatterns =[
       url(r'^login/$',views.login,name='login'),
       url(r'^logout/$' ,views.logout,name='logout'),
       url(r'^signup/$',signup, name='signup'),
-      url(r'^home/$',home,name='home'),
       url(r'^add-keyword/$', add_keyword, name='add_keyword'),
       url(r'^add-code/$', add_code, name='add_code'),
       url(r'^get-profile/$',get_profile,name='get_profile'),
       url(r'^admin/', admin.site.urls),
+      url(r'^$',home,name='home'),
 ]
 
 
